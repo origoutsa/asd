274fc8f6db9bc085ff06e75069d5e781ad0f4c71
Outsa <outsa@duck.com>
2025-09-25 13:55:36 +0200

    edited yaml

--- vue-project/vue.config.js
+++ vue-project/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+
+const path = require('path');
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  outputDir: path.resolve(__dirname, 'dist')
+});
